58f1a8cb4147a095cd6b3c6301eb622de831db43
Tim Gabets <tim@gabets.ru>
2017-10-26 15:21:29 +0300

    parse tryme

--- pyatm/tools.py
+++ pyatm/tools.py
@@ -1,2 +1,12 @@
 def parce_trace_data(data):
-	return b''
+  parsed = ''
+  if data: 
+    splitted = data.split('|')
+    for trace_string in splitted:
+      if trace_string[0] == ' ':
+        i = 0
+        while i < 16:
+          parsed += bytes('\\x' + trace_string[3*i+1 : 3*i+3], 'utf-8')
+          i += 1
+
+  return parsed
--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,11 @@ class TestParseTraceData(unittest.TestCase):
     def test_parce_trace_data(self):
         self.assertEqual(parce_trace_data(''), b'')
 
-    def test_parce_trace_data_transaction_request(self):
+    def test_parce_trace_single_string_data(self):
+      data = """13:51:42.580796 -| 31.31.1C.30.30.35.30.30.30.30.30.30.1C.1C.1C.31       11.005000000...1"""
+      self.assertEqual(parce_trace_data(data), b'')
+
+    def _test_parce_trace_data_transaction_request(self):
       data = """13:51:42.580796 -| 31.31.1C.30.30.35.30.30.30.30.30.30.1C.1C.1C.31       11.005000000...1
       13:51:42.580806 -| 31.1C.3B.36.30.33.37.39.39.31.30.31.32.33.34.35       1.;6037991012345
       13:51:42.580815 -| 36.37.34.3D.32.30.30.33.31.30.31.31.32.39.35.37       674=200310112957
